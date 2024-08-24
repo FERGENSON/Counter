9ad9363b332170a6fe65e36c3ebd3eb3aa04094c
Fergenson <fergenson.beats@yandex.ru>
2024-08-24 15:54:39 +0300

    Базовая задача решена. Добавлена Большая кнопка и счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countNumberUILabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    private var counter = 0
+    
+    @IBAction func bigButtonCount(_ sender: Any) {
+        counter += 1
+        countNumberUILabel.text = "Значение счетчика: \(counter)"
+    }
+    
 }
 
